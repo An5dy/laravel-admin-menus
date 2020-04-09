b27771f8c58d17d2a108db62b05ef33057cb7d4a
an5dy <846562014@qq.com>
2020-04-09 23:21:01 +0800

    Test StyleCI.

--- src/Console/ExportMenusCommand.php
+++ src/Console/ExportMenusCommand.php
@@ -25,8 +25,7 @@ class ExportMenusCommand extends Command
 
     protected $description = '导出 laravel-admin 菜单';
 
-    public function handle()
-    {
+    public function handle(){
         if (!$this->confirmToProceed()) {
             return 1;
         }
